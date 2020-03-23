cc7c9dca51f7997f6e355051ba32ef2f3c9ee0f2
Matthew Spana <spanam@bc.edu>
2020-03-23 19:28:55 -0400

    with .gitignore

---
+++ WeatherGift/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  WeatherGift
+//
+//  Created by Matthew Spana on 3/23/20.
+//  Copyright © 2020 Matthew Spana. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyB98VVm9kyjjywRTC12mv8KyYhD6NEF93w"
+}
