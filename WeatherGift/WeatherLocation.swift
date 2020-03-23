03d2b6658b39c08ab9b3ce8e0b2641e748eea0b9
Matthew Spana <spanam@bc.edu>
2020-03-23 19:01:40 -0400

    Section 7.2

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Matthew Spana on 3/23/20.
+//  Copyright © 2020 Matthew Spana. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
