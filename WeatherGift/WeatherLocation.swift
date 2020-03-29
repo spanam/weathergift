a20d47c77d1c69b4c601191dfee897ee3e42cd7c
Matthew Spana <spanam@bc.edu>
2020-03-29 17:27:24 -0400

    Section 7.10 - Added JSON call functions

--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -20,4 +20,41 @@ class WeatherLocation: Codable {
         self.longitude = longitude
     }
     
+    func getData() {
+        // Note: will work for any api URL
+        let coordinates = "\(latitude),\(longitude)"
+        let urlString = "\(APIurls.darkSkyURL)\(APIKeys.darkSkyKey)/\(coordinates)"
+        
+//        let urlString = "https://pokeapi.co/api/v2/pokemon/"
+        
+        print("\(urlString)")
+        
+        // Create URL
+        guard let url = URL(string: urlString) else {
+            print("ERROR: Could not create url from \(urlString)")
+            return
+        }
+        
+        // Create Session
+        let session = URLSession.shared
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("ERROR: \(error.localizedDescription)")
+            }
+            
+            // note: there are additional things that can go wrong, but we will ignore them for simplicity and because they're unlikely
+            
+            do {
+            let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                print("\(json)")
+            } catch {
+                print("JSON ERROR: \(error.localizedDescription)")
+            }
+            
+        }
+        
+        task.resume()
+        
+    }
+    
 }
