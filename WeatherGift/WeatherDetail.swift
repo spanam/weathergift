45fca7cd57da6b66de9a0beb9fe0ed30ce86f982
Matthew Spana <spanam@bc.edu>
2020-03-30 00:12:08 -0400

    7.14 - Added functionality for retrieving daily highs and lows

--- WeatherGift/WeatherDetail.swift
+++ WeatherGift/WeatherDetail.swift
@@ -8,22 +8,48 @@
 
 import Foundation
 
+private let dateFormatter: DateFormatter = {
+    print("Date Formatter Created in WeatherDetail.swift")
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "EEEE"
+    return dateFormatter
+}()
+
+struct DailyWeatherData: Codable {
+    var dailyIcon: String
+    var dailyWeekday: String
+    var dailySummary: String
+    var dailyHigh: Int
+    var dailyLow: Int
+}
+
 class WeatherDetail: WeatherLocation {
     
-    struct Result: Codable {
+    
+    
+    private struct Result: Codable {
         var timezone: String
         var currently: Currently
         var daily: Daily
     }
     
-    struct Currently: Codable {
+    private struct Currently: Codable {
         var temperature: Double
         var time: TimeInterval
     }
     
-    struct Daily: Codable {
+    private struct Daily: Codable {
         var summary: String
         var icon: String
+        var data: [DailyData]
+    }
+    
+    private struct DailyData: Codable {
+        var icon: String
+        var time: TimeInterval
+        var summary: String
+        var temperatureHigh: Double
+        var temperatureLow: Double
     }
     
     var timezone = ""
@@ -31,6 +57,7 @@ class WeatherDetail: WeatherLocation {
     var summary = ""
     var dailyIcon = ""
     var currentTime = 0.0
+    var dailyWeatherData: [DailyWeatherData] = []
     
     func getData(completed: @escaping () -> () ) {
         // Note: will work for any api URL
@@ -64,6 +91,18 @@ class WeatherDetail: WeatherLocation {
                 self.summary = result.daily.summary
                 self.dailyIcon = result.daily.icon
                 self.currentTime = result.currently.time
+                for index in 0..<result.daily.data.count {
+                    let weekdayDate = Date(timeIntervalSince1970: result.daily.data[index].time)
+                    dateFormatter.timeZone = TimeZone(identifier: result.timezone)
+                    let dailyWeekday = dateFormatter.string(from: weekdayDate)
+                    let dailyIcon = result.daily.data[index].icon
+                    let dailySummary = result.daily.data[index].summary
+                    let dailyHigh = Int(result.daily.data[index].temperatureHigh)
+                    let dailyLow = Int(result.daily.data[index].temperatureLow)
+                    let dailyWeather = DailyWeatherData(dailyIcon: dailyIcon, dailyWeekday: dailyWeekday, dailySummary: dailySummary, dailyHigh: dailyHigh, dailyLow: dailyLow)
+                    self.dailyWeatherData.append(dailyWeather)
+                    print("Day: \(dailyWeather.dailyWeekday) High: \(dailyWeather.dailyHigh) Low \(dailyWeather.dailyLow)")
+                }
             } catch {
                 print("JSON ERROR: \(error.localizedDescription)")
             }
